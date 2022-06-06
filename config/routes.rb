902421bf88c0c89b04fbe4fc05684a1e00b1c3ae
Tamara Dowis <tamara.dowis@gmail.com>
2022-06-05 20:15:09 -0500

    initial set-up with RSpec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        resources :affirmations, only: %i[index show]
+      end
+    end
+  end
 end
